6d28bb88c7e34cfbef0f0ce618949056c9eb2d4f
eBragastini <enri.braga@gmail.com>
2021-01-31 17:22:45 +0100

    aggiunta versione finale (da revisionare)

--- script.py
+++ script.py
@@ -5,11 +5,11 @@ f.write("")
 f.close()
 
 f = open("cmd.blif", "a")
-for n in range(random.randint(40, 70)):
-    for i in range(random.randint(0, 5)):
+for n in range(random.randint(40, 100)):
+    for i in range(random.randint(1, 3)):
         f.write("source script.rugged\n")
         
-    for i in range(random.randint(3, 10)):
+    for i in range(random.randint(8, 15)):
         f.write("map -m 0\n")
         
 f.write("map -s")
